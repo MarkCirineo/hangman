fec79045d48b1491d9d95abb77b3952bc5a34431
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-12-05 16:00:04 -0500

    created model of a User

---
+++ server/models/User.js
@@ -0,0 +1,57 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            require: true,
+            unique: true,
+        },
+        email: {
+            type:String,
+            require: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must use a valid email address'],
+        },
+        password: {
+            type: String,
+            require: true,
+        },
+        wins: {
+            type: Number,
+        },
+        losses: {
+            type: Number,
+        },
+        gamesPlayed: {
+            type: Number,
+        },
+        rating: {
+            type: Number,
+        },
+        team: {
+            type: Schema.Types.ObjectId,
+            ref: "Team",
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+userSchema.pre("save", async (next) => {
+    if (this.isNew || this.isModified("password")) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+    next();
+});
+
+userSchema.methods.isCorrectPassword = async (password) => {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = model("User", userSchema);
+
+module.exports = User;
