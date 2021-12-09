6e0a603c0a977cb2a51382a84fdf19068dd2ee48
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-12-09 15:58:50 -0500

    created user mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,33 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        loging(email: $email, password: $password) {
+            token
+            user {
+                _id 
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id 
+                username
+            }
+        }
+    }
+`;
+
+export const DELETE_USER = gql`
+    mutation deleteUser {
+        deleteUser {
+            _id
+        }
+    }
+`;
